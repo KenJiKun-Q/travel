c0ce4c68c72ffffea26f44e3506c25f398ee21c0
KenJiKun-Q <51771104+KenJiKun-Q@users.noreply.github.com>
2019-10-11 23:45:47 +0800

    头部脚部

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: "冲冲冲旅游网站",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
---
+++ pages/tab.vue
@@ -0,0 +1,58 @@
+<template>
+  <div style="padding:50px">
+
+      <div class="btns">
+          <span v-for="(item,index) in ['首页','中页','尾页']" :key="index"
+          @click="handleClick(index)"
+          :class="{ active: index === current }">
+            {{item}}
+          </span>
+      </div>
+
+        <div v-if="current === 0">
+            首页首页首页首页首页首页首页首页
+        </div>
+
+        <div v-if="current === 1">
+            中页中页中页中页中页中页中页中页
+        </div>
+
+
+        <div v-if="current === 2">
+            尾页尾页尾页尾页尾页尾页尾页尾页
+        </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            current:0
+        }
+    },
+    methods:{
+        handleClick(index){
+            this.current = index
+        }
+    }
+}
+</script>
+
+<style lang='less'>
+.btns{
+    margin-bottom: 20px;
+    span{
+        cursor: pointer;
+        border: 1px solid #333;
+        margin-right: 10px;
+        padding: 10px;
+    }
+
+    .active{
+        color: blue;
+        border-color: blue;
+    }
+}
+</style>
