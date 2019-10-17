10f88f433845ac4cbd4e0919b3b15134871a9475
KenJiKun-Q <51771104+KenJiKun-Q@users.noreply.github.com>
2019-10-17 16:56:19 +0800

    订单提交成功

--- components/air/orderForm.vue
+++ components/air/orderForm.vue
@@ -173,7 +173,11 @@ export default {
             this.$axios({
                 url:"/airorders",
                 method:"POST",
-                data
+                data,
+                headers:{
+                    // 这是jwt标准的token
+                    Authorization: `Bearer ${this.$store.state.user.userInfo.token}`
+                }
             }).then(res => {
                 console.log(res)
             })
