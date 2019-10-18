35337c414504580b5f09e3df1ce3c91790d09581
KenJiKun-Q <51771104+KenJiKun-Q@users.noreply.github.com>
2019-10-18 20:17:48 +0800

    生成二维码

--- pages/air/pay.vue
+++ pages/air/pay.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="container">
+        <script src="/qrcode.js"></script>
         <div class="main">
             <div class="pay-title">
-                支付总金额 <span class="pay-price">￥ 1000</span>
+                支付总金额 <span class="pay-price">￥ {{order.price}}</span>
             </div>
             <div class="pay-main">
                 <h4>微信支付</h4>
@@ -12,7 +13,8 @@
                 class="pay-qrcode">
                     <div class="qrcode">
                         <!-- 二维码 -->
-                        <canvas id="qrcode-stage"></canvas>
+                        <!-- <canvas id="qrcode-stage"></canvas> -->
+                        <div id="qrcode"></div>
                         <p>请使用微信扫一扫</p>
                         <p>扫描二维码支付</p>
                     </div>
@@ -37,15 +39,20 @@ export default {
         //订单id
         let {id} = this.$route.query;
 
-        //请求订单详情
-        this.$axios({
-            url:"/airorders/"+id,
-            headers:{
-                Authorization: `Bearer ${this.$store.state.user.userInfo.token}`
-            }
-        }).then(res=>{
-            this.order = res.data
-        })
+        //等待本地的插件把本地存储的值赋值到store,之后再执行请求,才可以拿到token
+        setTimeout(() => {
+            //请求订单详情
+            this.$axios({
+                url:"/airorders/"+id,
+                headers:{
+                    Authorization: `Bearer ${this.$store.state.user.userInfo.token}`
+                }
+            }).then(res=>{
+                this.order = res.data
+
+                new QRCode(document.getElementById("qrcode"),this.order.payInfo.code_url)
+            })
+        },10)
     }
 }
 </script>
