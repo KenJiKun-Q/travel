ee72acf1c804dc863fbdb7c48ce1e36655126031
KenJiKun-Q <51771104+KenJiKun-Q@users.noreply.github.com>
2019-10-19 19:11:02 +0800

    多选条件过滤

--- components/air/flightsFilters.vue
+++ components/air/flightsFilters.vue
@@ -14,7 +14,7 @@
             <!-- option里面的label是显示的界面上 -->
             <!-- 在下拉选项中选中值的时候取的value值 -->
             <el-col :span="4">
-                <el-select size="mini" v-model="airport" placeholder="起飞机场" @change="handleAirport">
+                <el-select size="mini" v-model="filters.airport" placeholder="起飞机场">
                     <el-option
                     v-for="(item,index) in data.options.airport"
                     :key="index"
@@ -25,7 +25,7 @@
                 </el-select>
             </el-col>
             <el-col :span="4">
-                <el-select size="mini" v-model="flightTimes"  placeholder="起飞时间" @change="handleFlightTimes">
+                <el-select size="mini" v-model="filters.flightTimes"  placeholder="起飞时间">
                     <el-option
                     v-for="(item,index) in data.options.flightTimes"
                     :key="index"
@@ -36,7 +36,7 @@
                 </el-select>
             </el-col>
             <el-col :span="4">
-                <el-select size="mini" v-model="company"  placeholder="航空公司" @change="handleCompany">
+                <el-select size="mini" v-model="filters.company"  placeholder="航空公司">
                     <el-option
                     v-for="(item,index) in data.options.company"
                     :key="index"
@@ -46,7 +46,7 @@
                 </el-select>
             </el-col>
             <el-col :span="4">
-                <el-select size="mini" v-model="airSize" placeholder="机型" @change="handleAirSize">
+                <el-select size="mini" v-model="filters.airSize" placeholder="机型" >
                     <el-option
                     v-for="(item,index) in sizeOptions"
                     :key="index"
@@ -74,10 +74,12 @@
 export default {
     data(){
         return {
-            airport: "",        // 机场
-            flightTimes: "",    // 出发时间
-            company: "",        // 航空公司
-            airSize: "",        // 机型大小
+            filters:{
+                airport: "",        // 机场
+                flightTimes: "",    // 出发时间
+                company: "",        // 航空公司
+                airSize: "",        // 机型大小
+            },
 
             //机型的列表
             sizeOptions:[
@@ -95,68 +97,56 @@ export default {
             default: {}
         }
     },
-    methods: {
-        // 选择机场时候触发
-        handleAirport(value){
-            // console.log(value)
-            //根据value过滤列表,只保留当前符合条件的机票列表
-            let arr = this.data.flights.filter(v => {
-                return v.org_airport_name === value;
-            })
+    watch:{
+        filters:{
+            deep:true,
+            handler(){
+                // 统一实现四个条件的过滤
+                // (重要：先假设全部的航班都是符合条件，从选中的条件里面找出不符合条件设置valid为false)
+                var arr = this.data.flights.filter(v => {
+                    // console.log(v)
+                    let valid = true;
 
-            //修改列表数据的
-            this.$emit('setDatalist',arr)
-        },
+                    //航空公司有选中值时候才判断
+                    if(this.filters.company && this.filters.company !== v.airline_name){
+                        valid = false;
+                    }
+                    //机场
+                    if(this.filters.airport && this.filters.airport !== v.org_airport_name){
+                        valid = false;
+                    }
+                    //过滤时间
+                    if(this.filters.flightTimes){
+                        //出发时间的小时
+                        let start = +v.dep_time.split(":")[0];
+                        // console.log(start)
+                        let arr = this.filters.flightTimes.split(",")
+                        // console.log(arr)
 
-        // 选择出发时间时候触发
-        handleFlightTimes(value){
-            //数组中第一项是开始事件,第二项是终止时间
-            // console.log(value)
-            let arr = value.split(",")
+                        //不在时间段内
+                        if(start < +arr[0] || start >= +arr[1]){
+                            valid = false
+                        }
+                    }
 
-            let arr2 = this.data.flights.filter(v => {
-                //出发时间的小时
-                let start = +v.dep_time.split(":")[0];
-                //比较航班出发时间是否在选中的事件段内
-                return start >= +arr[0] && start < +arr[1]
-            })
+                    if(this.filters.airSize && this.filters.airSize !== v.plane_size){
+                        valid = false;
+                    }
 
-            //修改列表数据的
-            this.$emit('setDataList',arr2)
-        },
 
-         // 选择航空公司时候触发
-        handleCompany(value){
-            // 根据value过滤列表,值保留当前符号条件的机票列表
-            let arr = this.data.flights.filter(v=>{
-                return v.airline_name === value;
-            })
-
-            //修改列表数据
-            this.$emit("setDataList",arr)
-        },
-
-         // 选择机型时候触发
-        handleAirSize(value){
-           let arr = this.data.flights.filter( v => {
-               return v.plane_size === value
-           })
-
-        //    修改列表数据
-        this.$emit("setDataList",arr)
-        },
-        
-        // 撤销条件时候触发
+                    return valid;
+                })
+                this.$emit("setDataList",arr)
+            }
+        }
+    },
+    methods:{
         handleFiltersCancel(){
+            console.log(123)
             this.airport = "";
-            this.flightTimes = "";
-            this.company = "";
-            this.airSize = "";
-
-            //传递没有修改的列表数据
-            this.$emit("setDataList",this.data.flights)
-        },
+        }
     }
+    
 }
 </script>
 
--- pages/hotel/index.vue
+++ pages/hotel/index.vue
@@ -1,15 +1,33 @@
 <template>
-  <div>
-      hotel
+  <div style="padding:50px">
+      <h3>高德地图</h3>
+      <div id="container" ></div>
   </div>
 </template>
 
 <script>
 export default {
-
+  mounted(){
+    window.onLoad  = function(){
+        var map = new AMap.Map('container', {
+        zoom:11,//级别
+        center: [116.397428, 39.90923],//中心点坐标
+        // viewMode:'3D'//使用3D视图
+    });
+  }
+  var url = 'https://webapi.amap.com/maps?v=1.4.15&key=6481c23fff9e9aeef30be9f0742acc63&callback=onLoad';
+  var jsapi = document.createElement('script');
+  jsapi.charset = 'utf-8';
+  jsapi.src = url;
+  document.head.appendChild(jsapi);
+  }
 }
+ 
 </script>
 
 <style>
-
+  #container {
+    width:500px; 
+    height: 380px;
+  }  
 </style>
