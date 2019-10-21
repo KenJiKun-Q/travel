dd917d4d716dd9d9e722cf555e9913465cc36496
zhongruiqiang <q730513957@163.com>
2019-10-21 15:04:54 +0800

    已添加旅游城市导航

--- components/post/postlist.vue
+++ components/post/postlist.vue
@@ -1,16 +1,26 @@
 <template>
   <div class="container">
     <div class="city-list">
-      <div class="city-list-top clearfix"></div>
+      <div class="city-list-top clearfix" style="font-size:0;">111</div>
       <div class="city-list-left clearfix" v-for="(item,index) in data" :key="index">
-        <a href="#" class="fl">{{item.type}}</a>
+        <a href="#" class="fl">
+          <a href="#">{{item.type}}</a>
+        </a>
         <i class="el-icon-arrow-right fr"></i>
-      </div>
-      <div class="city-list-right">
-        <div class="city-list-right-info">
-          <em>1</em>
-          <i>北京</i>
-          <span>世界著名古都和现代化国际城市</span>
+        <div class="city-list-right">
+          <div
+            class="city-list-right-info clearfix"
+            v-for="(list,index) in item.children"
+            :key="index"
+          >
+            <em class="fl">{{index+1}}</em>
+            <i class="fl">
+              <a href="#">{{list.city}}</a>
+            </i>
+            <span class="fl">
+              <a href="#">{{list.desc}}</a>
+            </span>
+          </div>
         </div>
       </div>
     </div>
@@ -47,30 +57,53 @@ export default {
         line-height: 41px;
         font-size: 20px;
       }
+
+      .city-list-right {
+        position: absolute;
+        width: 400px;
+        top: 20px;
+        left: 260px;
+        display: none;
+        background: #fff;
+        .city-list-right-info {
+          height: 41px;
+          line-height: 41px;
+          border-left: 1px #ddd solid;
+          border-right: 1px #ddd solid;
+          em {
+            font-style: italic;
+            color: #ffa500;
+            font-size: 28px;
+            font-weight: 400;
+            padding-left: 10px;
+          }
+          i {
+            color: #ffa500;
+            padding-left: 10px;
+          }
+          span {
+            padding: 0 10px;
+            color: 666;
+          }
+        }
+        .city-list-right-info:first-child {
+          border-top: 1px #ddd solid;
+        }
+        .city-list-right-info:last-child {
+          border-bottom: 1px #ddd solid;
+        }
+        .city-list-right-info:nth-child(-n + 4) {
+          border-left: none;
+        }
+      }
     }
-    .city-list-right {
-      position: absolute;
-      top: 20px;
-      left: 260px;
-      .city-list-right-info {
-        height: 41px;
-        line-height: 41px;
-        border-left: 1px #ddd solid;
-        border-right: 1px #ddd solid;
-        em {
-          font-style: italic;
-          color: #ffa500;
-          font-size: 28px;
-          font-weight: 400;
-          padding-left: 10px;
-        }
-        i {
-          color: #ffa500;
-          padding-left: 10px;
-        }
+    .city-list-left:hover {
+      border-right: none;
+      color: #ffa500;
+      .city-list-right {
+        display: block;
         span {
-          padding-left: 10px;
-          color: 666;
+          color: #999999;
         }
       }
     }
