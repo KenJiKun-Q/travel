a89a510c193582dd69e594002bfd28930de16503
tanjinlei <43749274+tanjinlei@users.noreply.github.com>
2019-10-21 16:02:09 +0800

    富文本框部分完成

--- pages/postadd/index.vue
+++ pages/postadd/index.vue
@@ -1,57 +1,110 @@
 <template>
-    <div style="padding:30px;">
-      <!--  -->
+  <div>
+    <!-- 标题 -->
+    <div style="margin-left:30px; weight:50%">
       <el-form :model="form" label-width="80px">
         <div class="title_word">
           <h2>发表新攻略</h2>
           <p>分享你的个人游记，让更多人看到哦！</p>
-        <el-input v-model="form.title" placeholder="请搜索游玩城市"></el-input>
+          <el-input v-model="form.title" placeholder="请输入标题"></el-input>
         </div>
-      <!-- </el-form> -->
-      <!--  -->
-    <!-- <el-form :model="form" label-width="80px"> -->
-      <el-form-item label="选择城市" class="formcity">
-        <el-input v-model="form.title" placeholder="请搜索游玩城市"  class="city_input"></el-input>
-      </el-form-item>
+
+        <!-- 中间富文本 -->
+        <div class="fuwenben">
+          <VueEditor :config="config" ref="vueEditor" style="height:500px; margin-bottom:20px;" />
+        </div>
+`
+        <!-- 底部搜索 -->
+        <el-form-item label="选择城市" class="formcity">
+          <el-input v-model="form.city" placeholder="请搜索游玩城市" class="city_input"></el-input>
+        </el-form-item>
+
+        <!-- 底部上传或者草稿部分 -->
+        <div class="sousuo">
+          <el-button size="small" type="primary">点击发布</el-button>或者
+          <a href="javascript:;">保存到草稿</a>
+        </div>
       </el-form>
-      </div>
+    </div>
+    <div class="right">123</div>
+  </div>
 </template>
 
 <script>
-import "quill/dist/quill.snow.css"
-let VueEditor;
-export default {
-  data () {
-    return {
-
-      // 提交的表单数据
-      form:{
-      title: "",     // 文章标题
-      content: "",   // 文章内容
-      city:"",       // 城市id（城市名称）
-      }
-      
-    }
-  }
-
+if (process.browser) {
+  VueEditor = require("vue-word-editor").default;
 }
+import "quill/dist/quill.snow.css";
+let VueEditor;
+
+export default {
+  data() {
+    return {
+      // 提交的表单数据
+      form: {
+        title: "", // 文章标题
+        content: "", // 文章内容
+        city: "" // 城市id（城市名称）
+      },
+      config: {
+        // 上传图片的配置
+        uploadImage: {
+          url: "http://localhost:3000/upload",
+          name: "file",
+          // res是结果，insert方法会把内容注入到编辑器中，res.data.url是资源地址
+          uploadSuccess(res, insert) {
+            insert("http://localhost:3000" + res.data.url);
+          }
+        },
+
+        // 上传视频的配置
+        uploadVideo: {
+          url: "http://localhost:3000/upload",
+          name: "file",
+          uploadSuccess(res, insert) {
+            insert("http://localhost:3000" + res.data.url);
+          }
+        }
+      }
+    };
+  },
+  components: {
+    VueEditor
+  }
+};
 </script>
 
 <style scoped lang="less">
-
-.title_word{
-  p{
+.title_word {
+  margin-bottom: 20px;
+  p {
     margin: 10px;
     font-size: 12px;
   }
 }
-
-.formcity{
+.fuwenben {
+  margin-bottom: 20px;
+}
+.formcity {
   margin-top: 20px;
-  .city_input{
-  width: 202px;
-  height: 40px;
+  .city_input {
+    width: 202px;
+    height: 40px;
+  }
+}
+.sousuo {
+  margin-bottom: 20px;
+
+  a {
+    color: yellow;
+    :hover {
+      text-decoration: underline;
+    }
   }
 }
 
+.right {
+  width: 50px;
+  height: 50px;
+}
 </style>
