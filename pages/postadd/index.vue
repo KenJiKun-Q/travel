e57aa9d69ba5d3d367852f88151a2a0f2e2e5f69
tanjinlei <43749274+tanjinlei@users.noreply.github.com>
2019-10-21 16:34:16 +0800

    页面渲染完成

--- pages/postadd/index.vue
+++ pages/postadd/index.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
+    <el-row>
+      <!--  -->
+      <el-col :span="17"><div class="grid-content bg-purple-dark">
     <!-- 标题 -->
-    <div style="margin-left:30px; weight:50%">
+    <div style="margin-left:30px; weight:500px;">
       <el-form :model="form" label-width="80px">
         <div class="title_word">
           <h2>发表新攻略</h2>
@@ -13,7 +16,7 @@
         <div class="fuwenben">
           <VueEditor :config="config" ref="vueEditor" style="height:500px; margin-bottom:20px;" />
         </div>
-`
+
         <!-- 底部搜索 -->
         <el-form-item label="选择城市" class="formcity">
           <el-input v-model="form.city" placeholder="请搜索游玩城市" class="city_input"></el-input>
@@ -26,7 +29,24 @@
         </div>
       </el-form>
     </div>
-    <div class="right">123</div>
+    </div>
+    </el-col>
+
+
+<el-col :span="7"><div class="grid-content bg-purple-dark">
+  <!--  -->
+    <div class="right">
+      草稿箱（1）
+      <br>
+      <span data-v-a7cc81fa="" class="iconfont el-icon-edit"></span>
+      <p>
+        2019-10-21
+      </p>
+    </div>
+    </div>
+    </el-col>
+
+    </el-row>
   </div>
 </template>
 
@@ -79,6 +99,7 @@ export default {
   margin-bottom: 20px;
   p {
     margin: 10px;
+    color: #f3bd72;
     font-size: 12px;
   }
 }
@@ -87,6 +108,7 @@ export default {
 }
 .formcity {
   margin-top: 20px;
+  margin-top: 100px;
   .city_input {
     width: 202px;
     height: 40px;
@@ -97,14 +119,26 @@ export default {
 
   a {
     color: yellow;
-    :hover {
-      text-decoration: underline;
-    }
+   
   }
+   a:hover {
+      border-bottom: 1px yellow solid;
+    }
 }
 
 .right {
-  width: 50px;
-  height: 50px;
+  margin: 50px 70px;
+  width: 178px;
+  height: 79px;
+  padding: 10px;
+  border: 1px red solid;
+
+  span{
+    margin-top: 10px;
+  }
+
+  p{
+    color:#999;
+  }
 }
 </style>
