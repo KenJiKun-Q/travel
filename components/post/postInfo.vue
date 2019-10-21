641f0c10984042cce75dd93e05c43c51de310fb9
zhongruiqiang <q730513957@163.com>
2019-10-22 01:09:12 +0800

    已添加文章分页

--- components/post/postInfo.vue
+++ components/post/postInfo.vue
@@ -1,60 +1,69 @@
 <template>
   <div>
     <div class="container">
-      <div class="city-top clearfix">
-        <input type="text" placeholder="请输入想去的地方，比如：'广州'" />
-        <i class="el-icon-search" @click="handleSearch"></i>
-      </div>
-      <div class="city-head clearfix">
-        <i>推荐:</i>
-        <span>
-          <a href="#">广州</a>
-        </span>
-        <span>
-          <a href="#">上海</a>
-        </span>
-        <span>
-          <a href="#">北京</a>
-        </span>
-      </div>
-      <div class="city-info clearfix">
-        <div class="city-info-left fl">推荐攻略</div>
-        <div class="city-info-right fr" @click="handleLink">
-          <i class="el-icon-edit"></i> 写游记
+      <div>
+        <!-- 多张图片布局 -->
+        <div class="city-content" v-if="post.images.length >= 3">
+          <div class="city-content-title">
+            <a href="#">{{post.title}}</a>
+          </div>
+          <div class="city-content-details">
+            <a href="#">{{post.summary}}</a>
+          </div>
+          <div class="city-content-image">
+            <a href="#">
+              <img v-for="(item,index) in post.images" :key="index" :src="item" v-if="index<3" alt />
+            </a>
+          </div>
+          <div class="city-content-info">
+            <span>
+              <i class="el-icon-location-outline"></i>
+              {{post.cityName}}
+            </span>
+            <span>by</span>
+            <a href="#">
+              <span>
+                <img :src="$axios.defaults.baseURL+post.account.defaultAvatar" alt />
+                {{post.account.nickname}}
+              </span>
+            </a>
+            <span>
+              <i class="el-icon-view"></i>
+              {{post.watch}}
+            </span>
+            <em class="zan fr">15赞</em>
+          </div>
         </div>
-      </div>
-      <div class="city-content">
-        <div class="city-content-title">
-          <a href="#">塞班贵？一定是你的打开方式不对！6000块玩转塞班</a>
-        </div>
-        <div class="city-content-details">
-          <a href="#">
-            大家对塞班岛总存在着这样的误解，知道它是美属地盘，就理所当然地觉得这里的花费一定很高，
-            花费高有高的玩法，那如果只有6000块的预算呢？要怎么玩？关于旅行这件事，
-            我们要让钱花得更有道理，收下这份攻略，带你6000块花式玩转塞班。图：塞班岛。
-            by第5季旅游一、怎样用6000块玩转塞班？大多数出境游客人不做预算或最
-          </a>
-        </div>
-        <div class="city-content-image">
-          <a href="#">
-            <img src="../../assets/pic_sea.jpeg" alt />
-          </a>
-          <a href="#">
-            <img src="../../assets/pic_sea.jpeg" alt />
-          </a>
-          <a href="#">
-            <img src="../../assets/pic_sea.jpeg" alt />
-          </a>
-        </div>
-        <div class="city-content-info">
-          <span>
-            <i class="el-icon-location-outline"></i> 北京市
-          </span>
-          <span>by</span>
-          <span>地球发动机</span>
-          <span>
-            <i class="el-icon-view"></i> 8412
-          </span>
+        <!-- 单张图片布局 -->
+        <div
+          class="city-contented clearfix"
+          v-if="post.images.length > 0 && post.images.length < 3"
+        >
+          <div class="city-contented-left fl">
+            <div class="image">
+              <img :src="post.images[0]" alt />
+            </div>
+          </div>
+          <div class="city-contented-right fl">
+            <div class="contented-right-tittle">{{post.title}}</div>
+            <div class="contented-right-details">{{post.summary}}</div>
+            <div class="contented-right-info">
+              <span>
+                <i class="el-icon-location-outline"></i>
+                {{post.cityName}}
+              </span>
+              <span>by</span>
+              <a href="#">
+                <span>
+                  <img :src="$axios.defaults.baseURL+post.account.defaultAvatar" alt />地球发动机
+                </span>
+              </a>
+              <span>
+                <i class="el-icon-view"></i> 8412
+              </span>
+              <em class="zan fr">15赞</em>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -63,13 +72,10 @@
 
 <script>
 export default {
-  methods: {
-    handleSearch() {
-      console.log(123);
-    },
-    handleLink() {
-      console.log(123);
-    }
+  props: ["post"],
+  methods: {},
+  mounted() {
+    console.log(this.post);
   }
 };
 </script>
@@ -77,54 +83,13 @@ export default {
 <style scoped lang="less">
 .container {
   box-sizing: border-box;
-  margin: 20px 0 0 10px;
+  margin: 10px 0 0 10px;
   width: 700px;
-  .city-top {
-    position: relative;
-    input {
-      box-sizing: border-box;
-      width: 100%;
-      padding: 10px 0;
-      border: 3px #ffa500 solid;
-      text-indent: 2em;
-    }
-    i {
-      position: absolute;
-      font-size: 24px;
-      right: 14px;
-      top: 8px;
-      font-weight: 300;
-      color: #ffa500;
-    }
-  }
-  .city-head {
-    margin: 10px 0;
-    font-size: 14px;
-    color: #999;
-    span {
-      padding: 0 5px;
-    }
-  }
-  .city-info {
-    margin-top: 20px;
-    text-align: center;
-    border-bottom: 1px #ddd solid;
 
-    .city-info-left {
-      font-size: 20px;
-      color: #ffa500;
-      padding-bottom: 20px;
-      border-bottom: 3px #ffa500 solid;
-    }
-    .city-info-right {
-      padding: 10px 20px;
-      background: #409eff;
-      color: #fff;
-      border-radius: 5px;
-    }
-  }
   .city-content {
     margin-bottom: 20px;
+    padding: 10px 0;
+    border-bottom: 1px #ddd solid;
     .city-content-title {
       margin: 20px 0;
       font-size: 20px;
@@ -140,8 +105,12 @@ export default {
     }
     .city-content-image {
       width: 100%;
+
       img {
         width: 32.5%;
+        height: 150px;
+        padding-right: 5px;
+        object-fit: cover;
       }
     }
     .city-content-info {
@@ -150,6 +119,77 @@ export default {
       font-size: 13px;
       span {
         padding-right: 10px;
+      }
+      a {
+        img {
+          width: 14px;
+          height: 14px;
+          border-radius: 50%;
+          padding-right: 3px;
+        }
+        * {
+          vertical-align: middle;
+        }
+        span {
+          color: #ffa500;
+        }
+      }
+      em {
+        font-size: 16px;
+        color: #ffa500;
+      }
+    }
+  }
+  .city-contented {
+    margin: 20px 0;
+    padding: 10px 0;
+    border-bottom: 1px #ddd solid;
+    .city-contented-left {
+      width: 220px;
+      img {
+        width: 100%;
+      }
+    }
+    .city-contented-right {
+      margin-left: 10px;
+      .contented-right-tittle {
+        font-size: 19px;
+        width: 450px;
+        margin-bottom: 10px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 1;
+      }
+      .contented-right-details {
+        width: 450px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 3;
+      }
+      .contented-right-info {
+        padding-top: 15px;
+        font-size: 13px;
+        color: #999999;
+        a {
+          color: #ffa500;
+          img {
+            padding-right: 3px;
+            width: 14px;
+            height: 14px;
+            border-radius: 50%;
+          }
+        }
+        span {
+          padding-right: 10px;
+        }
+        em {
+          font-size: 16px;
+          color: #ffa500;
+        }
       }
     }
   }
--- pages/post/index.vue
+++ pages/post/index.vue
@@ -3,12 +3,48 @@
     <el-row class="w">
       <el-col :span="7">
         <div class="content-left">
+          <!-- 城市搜索导航 -->
           <PostList :data="cityList" />
         </div>
       </el-col>
       <el-col :span="17">
+        <div class="city-top clearfix">
+          <input type="text" placeholder="请输入想去的地方，比如：'广州'" />
+          <i class="el-icon-search" @click="handleSearch"></i>
+        </div>
+        <div class="city-head clearfix">
+          <i>推荐:</i>
+          <span>
+            <a href="#">广州</a>
+          </span>
+          <span>
+            <a href="#">上海</a>
+          </span>
+          <span>
+            <a href="#">北京</a>
+          </span>
+        </div>
+        <div class="city-info clearfix">
+          <div class="city-info-left fl">推荐攻略</div>
+          <div class="city-info-right fr" @click="handleLink">
+            <i class="el-icon-edit"></i> 写游记
+          </div>
+        </div>
         <div class="content-right">
-          <PostInfo />
+          <!-- 文章列表组件 -->
+          <PostInfo v-for="(item,index) in dataList" :key="index" :post="item" />
+
+          <!-- 分页 -->
+          <el-pagination
+            v-if="cityList.length"
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            :current-page="pageIndex"
+            :page-sizes="[2, 4, 8, 12]"
+            :page-size="pageSize"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="postlist.length"
+          ></el-pagination>
         </div>
       </el-col>
     </el-row>
@@ -19,14 +55,43 @@
 import PostList from "@/components/post/postlist";
 import PostInfo from "@/components/post/postInfo";
 export default {
+  computed: {
+    // 从data总列表中切割出来的数组列表
+    dataList() {
+      let arr = this.postlist.slice(
+        (this.pageIndex - 1) * this.pageSize,
+        this.pageIndex * this.pageSize
+      );
+      return arr;
+    }
+  },
   data() {
     return {
-      cityList: {}
+      cityList: {},
+      postlist: [],
+      pageSize: 2,
+      pageIndex: 1,
+      total: 0
     };
   },
   components: {
     PostList,
     PostInfo
+  },
+  methods: {
+    handleSearch() {
+      console.log(123);
+    },
+    handleLink() {
+      console.log(123);
+    },
+    handleSizeChange(val) {
+      // 切换条数
+      this.pageSize = val;
+    },
+    handleCurrentChange(val) {
+      this.pageIndex = val;
+    }
   },
   mounted() {
     this.$axios({
@@ -36,6 +101,15 @@ export default {
       const { data } = res.data;
       this.cityList = data;
     });
+
+    this.$axios({
+      url: "/posts"
+    }).then(res => {
+      console.log(res.data);
+      const { data } = res.data;
+      this.postlist = data;
+      console.log(this.postlist);
+    });
   }
 };
 </script>
@@ -44,5 +118,51 @@ export default {
 .el-row {
   width: 1000px;
   margin: 0 auto;
+
+  .city-top {
+    margin-top: 20px;
+    position: relative;
+    input {
+      box-sizing: border-box;
+      width: 100%;
+      padding: 10px 0;
+      border: 3px #ffa500 solid;
+      text-indent: 2em;
+    }
+    i {
+      position: absolute;
+      font-size: 24px;
+      right: 14px;
+      top: 8px;
+      font-weight: 300;
+      color: #ffa500;
+    }
+  }
+  .city-head {
+    margin: 10px 0;
+    font-size: 14px;
+    color: #999;
+    span {
+      padding: 0 5px;
+    }
+  }
+  .city-info {
+    margin-top: 20px;
+    text-align: center;
+    border-bottom: 1px #ddd solid;
+
+    .city-info-left {
+      font-size: 20px;
+      color: #ffa500;
+      padding-bottom: 20px;
+      border-bottom: 3px #ffa500 solid;
+    }
+    .city-info-right {
+      padding: 10px 20px;
+      background: #409eff;
+      color: #fff;
+      border-radius: 5px;
+    }
+  }
 }
 </style>
