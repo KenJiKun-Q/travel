084a07f3b727a3b074bd15aea433039a1979eb76
KenJiKun-Q <51771104+KenJiKun-Q@users.noreply.github.com>
2019-10-22 21:12:08 +0800

    攻略评论发表连接接口,设置楼层基本样式

--- components/post/postCommItem.vue
+++ components/post/postCommItem.vue
@@ -1,103 +1,47 @@
 <template>
-  <div class="comment">
-    <div class="comment-info" v-for="(item,index) in content" :key="index">
-      <div class="comment-user">
-        <!-- 头像 -->
-        <img src="../../assets/avatar.jpg" alt />
-        <!-- 用户名字 -->
-        <em>团灭发动机</em>
-        <span>2019-05-22 10:30</span>
-      </div>
-      <div class="qian-info">
-        <div class="qian-user">宇宙发动机</div>
-        <div class="qian-comment">冲冲冲</div>
-        <img src="../../assets/avatar.jpg" alt />
-        <div class="qian-reply">回复</div>
-      </div>
-      <div class="comment-neirou">
-        <div class="comment-font">{{item.content}}</div>
-        <span class="reply">回复</span>
+  <div>
+    <!-- 评论模块 -->
+    <div class="comment">
+      <div class="comment-info">
+        <div class="comment-user">
+          <img src="../../assets/avatar.jpg" alt />
+          <em>团灭发动机</em>
+          <span>2019-05-22 10:30</span>
+        </div>
+        <div class="comment-content">
+          <PostCommFloor/>
+          <p>hhhhhhhhhh</p>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import PostCommFloor from "@/components/post/postCommFloor"
 export default {
-    data(){
-        return{
-            content:[]
-        }
-    },
-    mounted(){
-        this.$axios({
-            url:"/posts/comments"
-        }).then(res =>{
-            // console.log(res)
-            let {data} = res.data
-            // console.log(data)
-            this.content = data
-            console.log(this.content)
-        })
-    }
+  components:{
+    PostCommFloor
+  }
 };
 </script>
 
 <style scoped lang="less">
-
-.comment {
-  padding: 15px;
-  border: 1px solid #dddddd;
-  margin-top: 30px;
-  .comment-info {
-    // margin-bottom: 15px;
-    position: relative;
-    .comment-user {
-      align-items: center;
-      margin-bottom: 20px;
-      em {
+.comment{
+  padding: 20px;
+  border: 1px solid #999;
+  .comment-info{
+    .comment-user{
+      
+      em{
         font-size: 13px;
       }
-      span {
+      span{
         font-size: 12px;
-        color: #b39999;
       }
-      img {
+      img{
+        vertical-align:middle;
         width: 20px;
-        border-radius: 50%;
-        vertical-align: middle;
-      }
-    }
-    .reply {
-      position: absolute;
-      right: 0;
-      bottom: 0;
-    }
-    .comment-neirou {
-      margin-top: 20px;
-      font-size: 13px;
-    }
-    .qian-info {
-      margin-left: 35px;
-      padding: 15px;
-      border: 1px solid #dddddd;
-      width: 600px;
-      background-color: #f9f9f9;
-      font-size: 13px;
-      position: relative;
-      .qian-comment {
-        padding: 15px 0;
-      }
-      img {
-        padding: 5px;
-        border: 1px dashed #dddddd;
-        width: 80px;
-        height: 80px;
-      }
-      .qian-reply {
-        position: absolute;
-        right: 11px;
-        bottom: 11px;
       }
     }
   }
